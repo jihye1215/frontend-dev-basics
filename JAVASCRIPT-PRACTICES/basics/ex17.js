85fbe87cb5b68e84a1939ff9b45699af33add4fa
jihye1215 <wlgp1494@naver.com>
2022-02-22 11:20:30 +0900

    js practices #10

---
+++ JAVASCRIPT-PRACTICES/basics/ex17.js
@@ -0,0 +1,8 @@
+console.log("=== global object =================");
+
+global.name = "둘리";
+console.log(name, global.name); 
+
+// cf
+email = "dooly@google.com";
+console.log(email, global.email); 
